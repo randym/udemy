0455d1fc12ba0115c359bb6846b7f82489b5477e
Randy Morgan <randy@Randys-iMac.local>
2021-04-30 03:33:27 +0900

    up to lecture 65

--- unrealcourse/cows_and_bulls/Source/BullCowGame/BullCowCartridge.cpp
+++ unrealcourse/cows_and_bulls/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,106 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(Intro); 
+    PrintLine(StartPrompt); 
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
+    if(isGameActive == false) {
+        Input == "Y" ? UBullCowCartridge::StartNewGame() : PrintLine(StartPrompt); 
+        return;
+    } 
+
+    UBullCowCartridge::HandleGuess(Input);
 }
+
+
+void UBullCowCartridge::StartNewGame()
+{
+    PrintLine(Instruction);
+    PrintLine(Guess);
+    PrintLine("");
+    PrintLine(Motivate); 
+
+
+    UBullCowCartridge::SelectIsogram();
+    Lives = HiddenWord.Len();
+
+    UBullCowCartridge::ShowLives();
+    isGameActive = true;
+
+    Cows = 0;
+    Bulls = 0;
+}
+
+void UBullCowCartridge::TryAgain() const
+{
+    PrintLine(Demotivate);
+    PrintLine(Failure);
+    UBullCowCartridge::ShowLives();
+    PrintLine(Guess);
+    UBullCowCartridge::ShowHint();
+}
+
+
+void UBullCowCartridge::EndGame(const bool isWon)
+{
+    isGameActive = false;
+
+    isWon ? PrintLine(YouWin) : PrintLine(YouLose);
+
+    PrintLine(FString::Printf(TEXT("The isogram was %s"), *HiddenWord));
+    PrintLine("");
+    PrintLine(StartPrompt);
+}
+
+void UBullCowCartridge::SelectIsogram() {
+    HiddenWord = TEXT("surge");
+}
+
+bool UBullCowCartridge::IsIsogram(const FString& Word) const
+{
+  for(int i = 0; i < Word.Len(); i++)
+  {
+    const char letter = Word[i];
+    for(int j = i + 1; j < Word.Len(); j++) {
+      if(Word[j] == letter) {
+          return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+void UBullCowCartridge::HandleGuess(const FString& UserGuess) {
+    if(UserGuess.ToLower() == HiddenWord.ToLower()) {
+        UBullCowCartridge::EndGame(true);
+        return;
+    }
+
+    bool isSameLength = UserGuess.Len() == HiddenWord.Len();
+    bool isIsogram = UBullCowCartridge::IsIsogram(UserGuess)
+
+    if(--Lives == 0) {
+        UBullCowCartridge::EndGame(false);
+        return;
+    }
+
+    UBullCowCartridge::TryAgain();
+}
+
+void UBullCowCartridge::ShowHint() const
+{
+    const FString Notice = FString::Printf(TEXT("(hint: %i letters long)"), HiddenWord.Len());
+    PrintLine(Notice);
+}
+
+void UBullCowCartridge::ShowLives() const
+{
+    PrintLine(FString::Printf(TEXT("You only have %i lives!"), Lives));
+}
+
--- unrealcourse/cows_and_bulls/Source/BullCowGame/BullCowCartridge.h
+++ unrealcourse/cows_and_bulls/Source/BullCowGame/BullCowCartridge.h
@@ -12,10 +12,41 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	GENERATED_BODY()
 
 	public:
+
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
 
 	// Your declarations go below!
 	private:
-	
+
+	void StartNewGame();
+	void HandleGuess(const FString& UserGuess);
+	void TryAgain() const;
+	void EndGame(const bool isWon);
+	void SelectIsogram();
+	void ShowHint() const;
+	void ShowLives() const;
+    bool IsIsogram(const FString& Word) const;
+	bool isGameActive = false;
+
+    FString HiddenWord;
+	int32 Lives, Cows, Bulls;
+
+	const FString Intro = TEXT("Howdy, partner...\nGot a hankering for some cows and bulls?");
+	const FString StartPrompt = TEXT("Type 'Y' and hit enter to play.");
+
+	const FString Motivate = TEXT("C'mon now, get a mooooove on");
+	const FString Demotivate = TEXT("Aw, fooey!");
+
+	const FString Instruction = TEXT("Try and guess the isogram");
+
+	const FString Failure = TEXT("Looks like you got it wrong hoss.");
+	const FString Success = TEXT("Nice one, Cowboy!");
+
+	const FString Guess = TEXT("Type your guess and press Enter");
+
+	const FString YouLose = TEXT("....shot and left for dead...");
+    const FString YouWin = TEXT("Nice one, Cowboy. Jangle those spurs!");
+
+	const FString GameOver = TEXT("Game Over");
 };
