9594996fa6d5d3bd9e3b7e351098deae90c73f0d
randym <digital.ipseity@gmail.com>
2021-05-01 03:12:11 +0900

    complete cows_and_bulls

--- unrealcourse/cows_and_bulls/Source/BullCowGame/BullCowCartridge.cpp
+++ unrealcourse/cows_and_bulls/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,110 +1,135 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
+#include <ctime>
 
-void UBullCowCartridge::BeginPlay() // When the game starts
-{
-    Super::BeginPlay();
-    PrintLine(Intro);
-    PrintLine(StartPrompt);
+void UBullCowCartridge::BeginPlay() {
+  Super::BeginPlay();
+  srand(time(NULL));
+
+  UBullCowCartridge::LoadWords();
+
+  PrintLine(Intro);
+  PrintLine(StartPrompt);
 }
 
-void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
-{
-    ClearScreen();
+void UBullCowCartridge::LoadWords() {
+  const FString Path = FPaths::ProjectContentDir() / Isograms;
 
-    if (isGameActive == false)
-    {
-        Input == "Y" ? UBullCowCartridge::StartNewGame() : PrintLine(StartPrompt);
-        return;
-    }
-
-    UBullCowCartridge::HandleGuess(Input);
+  FFileHelper::LoadFileToStringArray(Words, *Path);
+  UBullCowCartridge::ShuffleWords();
 }
 
-void UBullCowCartridge::StartNewGame()
-{
-    PrintLine(Instruction);
-    PrintLine(Guess);
-    PrintLine("");
-    PrintLine(Motivate);
+// HA! 77 shows me that Unreal has its own thing...
+// got ahead of my self but yeah -.. this is still
+// better as it shuffles once, and then just pulls
+// in order instead of doing a random range every time.
+// And then in 78 they do the right thing, kinda...
+// Still randoming every time but at least they cache
+// the culled isograms. Someone tell these people that
+// if you dont put garbage in, you wont get garbage out...
+void UBullCowCartridge::ShuffleWords() {
+  int32 i = Words.Num();
 
-    UBullCowCartridge::SelectIsogram();
-    Lives = HiddenWord.Len();
-    UBullCowCartridge::ShowLives();
-    isGameActive = true;
-
-    Cows = 0;
-    Bulls = 0;
+  while (--i) {
+    Words.Swap(i, rand() % (i + 1));
+  }
 }
 
-void UBullCowCartridge::TryAgain() const
-{
-    PrintLine(Demotivate);
-    PrintLine(Failure);
-    UBullCowCartridge::ShowLives();
-    PrintLine(Guess);
-    UBullCowCartridge::ShowHint();
+void UBullCowCartridge::OnInput(const FString &PlayerInput) {
+  ClearScreen();
+
+  if (isGameActive) {
+    UBullCowCartridge::HandleGuess(PlayerInput);
+    return;
+  }
+
+  PlayerInput == Affirmative ? UBullCowCartridge::StartNewGame()
+                             : PrintLine(StartPrompt);
 }
 
-void UBullCowCartridge::EndGame(const bool isWon)
-{
-    isGameActive = false;
+void UBullCowCartridge::StartNewGame() {
+  UBullCowCartridge::SetIsogram();
+  UBullCowCartridge::ShowLives();
+  isGameActive = true;
 
-    isWon ? PrintLine(YouWin) : PrintLine(YouLose);
-
-    PrintLine(TEXT("The isogram was %s"), *HiddenWord);
-    PrintLine("");
-    PrintLine(StartPrompt);
+  PrintLine(Instruction);
+  PrintLine(Guess);
+  PrintLine("");
+  PrintLine(Motivate);
 }
 
-void UBullCowCartridge::SelectIsogram()
-{
-    HiddenWord = TEXT("surge");
-}
+void UBullCowCartridge::HandleGuess(const FString &UserGuess) {
+  if (UserGuess == CheatCode) {
+    PrintLine(TEXT("CHEATER: %s"), *HiddenWord);
+    return;
+  }
 
-bool UBullCowCartridge::IsIsogram(const FString &Word) const
-{
-    for (int i = 0; i < Word.Len(); i++)
-    {
-        const char letter = Word[i];
-        for (int j = i + 1; j < Word.Len(); j++)
-        {
-            if (Word[j] == letter)
-            {
-                return false;
-            }
-        }
-    }
+  if (UserGuess.ToLower() == HiddenWord.ToLower()) {
+    UBullCowCartridge::EndGame(true);
+    return;
+  }
 
-    return true;
-}
-
-void UBullCowCartridge::HandleGuess(const FString &UserGuess)
-{
-    if (UserGuess.ToLower() == HiddenWord.ToLower())
-    {
-        UBullCowCartridge::EndGame(true);
-        return;
-    }
-
-    bool isSameLength = UserGuess.Len() == HiddenWord.Len();
-    bool isIsogram = UBullCowCartridge::IsIsogram(UserGuess);
-
-    if (--Lives == 0)
-    {
-        UBullCowCartridge::EndGame(false);
-        return;
-    }
-
+  if (--Lives) {
+    UBullCowCartridge::Score(UserGuess);
     UBullCowCartridge::TryAgain();
+    return;
+  }
+
+  UBullCowCartridge::EndGame(false);
 }
 
-void UBullCowCartridge::ShowHint() const
-{
-    PrintLine(TEXT("(hint: %i letters long)"), HiddenWord.Len());
+void UBullCowCartridge::Score(const FString &UserGuess) {
+  int32 Bulls = 0;
+  int32 Cows = 0;
+  int32 i = 0;
+
+  TArray<char16_t> Completion;
+
+  for (; i < UserGuess.Len() && i < HiddenWord.Len(); i++) {
+    if (UserGuess[i] == HiddenWord[i]) {
+      Completion.Emplace(UserGuess[i]);
+      Bulls++;
+      continue;
+    }
+
+    Completion.Emplace('_');
+    HiddenWord.Find(&UserGuess[i]) != -1 && Cows++;
+  }
+  PrintLine(TEXT("You said: %s"), *UserGuess);
+  PrintLine(TEXT("HANGMAN: %s"), *FString(Completion));
+  PrintLine(TEXT("You have %i Bulls, and %i Cows"), Bulls, Cows);
 }
 
-void UBullCowCartridge::ShowLives() const
-{
-    PrintLine(TEXT("You only have %i lives!"), Lives);
+void UBullCowCartridge::TryAgain() {
+  UBullCowCartridge::ShowLives();
+  PrintLine(Demotivate);
+  PrintLine(Failure);
+  PrintLine(Guess);
+  UBullCowCartridge::ShowHint();
+}
+
+void UBullCowCartridge::EndGame(const bool isWon) {
+  isGameActive = false;
+
+  isWon ? PrintLine(YouWin) : PrintLine(YouLose);
+
+  PrintLine(TEXT("The isogram was %s"), *HiddenWord);
+  PrintLine("");
+  PrintLine(StartPrompt);
+}
+
+void UBullCowCartridge::SetIsogram() {
+  // HiddenWord = Words[FMath::RandRange(0, Words.Num() - 1)]; -- erm ... no ...
+  HiddenWord = Words[WordIndex++];
+  Lives = HiddenWord.Len();
+}
+
+void UBullCowCartridge::ShowHint() const {
+  PrintLine(TEXT("(hint: %i letters long)"), HiddenWord.Len());
+}
+
+void UBullCowCartridge::ShowLives() const {
+  PrintLine(TEXT("Lives Remaining: %i"), Lives);
 }
--- unrealcourse/cows_and_bulls/Source/BullCowGame/BullCowCartridge.h
+++ unrealcourse/cows_and_bulls/Source/BullCowGame/BullCowCartridge.h
@@ -2,49 +2,55 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Console/Cartridge.h"
+#include "CoreMinimal.h"
 #include "BullCowCartridge.generated.h"
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
-class BULLCOWGAME_API UBullCowCartridge : public UCartridge
-{
-	GENERATED_BODY()
+class BULLCOWGAME_API UBullCowCartridge : public UCartridge {
+  GENERATED_BODY()
 
-public:
-	virtual void BeginPlay() override;
-	virtual void OnInput(const FString &Input) override;
+ public:
+  virtual void BeginPlay() override;
+  virtual void OnInput(const FString& PlayerInput) override;
 
-	// Your declarations go below!
-private:
-	void StartNewGame();
-	void HandleGuess(const FString &UserGuess);
-	void TryAgain() const;
-	void EndGame(const bool isWon);
-	void SelectIsogram();
-	void ShowHint() const;
-	void ShowLives() const;
-	bool IsIsogram(const FString &Word) const;
-	bool isGameActive = false;
+  // Your declarations go below!
+ private:
+  void LoadWords();
+  void ShuffleWords();
+  void StartNewGame();
+  void SetIsogram();
 
-	FString HiddenWord;
-	int32 Lives, Cows, Bulls;
+  void HandleGuess(const FString& UserGuess);
+  void TryAgain();
+  void Score(const FString& UserGuess);
+  void EndGame(const bool isWon);
 
-	const FString Intro = TEXT("Howdy, partner...\nGot a hankering for some cows and bulls?");
-	const FString StartPrompt = TEXT("Type 'Y' and hit enter to play.");
+  void ShowHint() const;
+  void ShowLives() const;
 
-	const FString Motivate = TEXT("C'mon now, get a mooooove on");
-	const FString Demotivate = TEXT("Aw, fooey!");
+  bool isGameActive = false;
 
-	const FString Instruction = TEXT("Try and guess the isogram");
+  TArray<FString> Words;
+  FString HiddenWord;
+  int32 Lives = 0, WordIndex = 0;
 
-	const FString Failure = TEXT("Looks like you got it wrong hoss.");
-	const FString Success = TEXT("Nice one, Cowboy!");
+  const FString CheatCode = TEXT("MOMMY");
+  const FString Isograms = TEXT("Isograms/isograms.txt");
+  const FString Affirmative = TEXT("Y");
+  const FString Intro =
+      TEXT("Howdy, partner...\nGot a hankering for some cows and bulls?");
+  const FString StartPrompt = TEXT("Type 'Y' and hit enter to play.");
 
-	const FString Guess = TEXT("Type your guess and press Enter");
+  const FString Motivate = TEXT("C'mon now, get a mooooove on");
+  const FString Demotivate = TEXT("Aw, fooey!");
 
-	const FString YouLose = TEXT("....shot and left for dead...");
-	const FString YouWin = TEXT("Nice one, Cowboy. Jangle those spurs!");
+  const FString Instruction = TEXT("Try and guess the isogram");
 
-	const FString GameOver = TEXT("Game Over");
+  const FString Failure = TEXT("Looks like you got it wrong hoss.");
+
+  const FString Guess = TEXT("Type your guess and press Enter");
+
+  const FString YouLose = TEXT("....shot and left for dead...");
+  const FString YouWin = TEXT("Nice one, Cowboy. Jangle those spurs!");
 };
